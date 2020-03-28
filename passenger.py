42e79f636663f950b77bb29d7405767f8475330e
Ben-Caruso <bcaruso@uvm.edu>
2020-03-28 15:29:25 -0400

    Added getters/setters for passenger.py

--- passenger.py
+++ passenger.py
@@ -5,3 +5,17 @@ class Passenger:
     def __init__(self):
         self.name = ""
         self.flight = Flight()
+
+    # Define getters
+    def get_name(self):
+        return self.name
+
+    def get_flight(self):
+        return self.flight
+
+    # Define setters
+    def set_name(self, new_name):
+        self.name = new_name
+
+    def set_flight(self, new_flight):
+        self.flight = new_flight
