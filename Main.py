a1a4fdd0cd48fd71085dfb2912a0eb7cfa2eeda3
jvoynow <voynow99@gmail.com>
2020-04-01 20:31:43 -0400

    Added array of names to become our passengers

--- Main.py
+++ Main.py
@@ -3,9 +3,24 @@ from passenger import Passenger
 from flight import Flight
 from airport import Airport
 
+import pandas as pd
+import numpy as np
 
 def main():
     airport_names = ['JFK', 'LAX', 'ATL', 'DFW']
+    # string flight_num, string airline, string date, string time
+    flight_info = [["UA1215", "United Airlines", "3/20/2020", "3:30pm"],
+                   ["UA1438", "United Airlines", "3/20/2020", "3:45pm"],
+                   ["UA3455", "United Airlines", "3/20/2020", "4:00pm"],
+                   ["UA4234", "United Airlines", "3/20/2020", "4:15pm"]]
+
+    # read_csv of names
+    df = pd.read_csv('baby_names.csv')
+    # get name values
+    names = df['NAME'].values
+    # pick 400 names with replacement
+    names = names[np.random.choice(len(names), 400)]
+    # these will become Passenger()'s
 
     airports = initialize_airports(airport_names)
     for i in range(len(airports)):
@@ -16,6 +31,10 @@ def main():
             flight.set_current_airport(airports[i])
             flight.set_destination_airport(
                 airports[i+1] if i < len(airports)-1 else airports[0])
+            flight.set_flight_number(flight_info[i][0])
+            flight.set_airline(flight_info[i][1])
+            flight.set_flight_date(flight_info[i][2])
+            flight.set_flight_time(flight_info[i][3])
 
     print_data(airports)
 
